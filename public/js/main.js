581a5eb7e8bdb0392860f0999855f6628bef2a4f
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2022-04-29 12:39:46 -0500

    refactor: enhance main, aplly css variables an use better image for performance

--- public/js/main.js
+++ public/js/main.js
@@ -1,44 +1,27 @@
-const breakpoints = {
-  sm: 700
-};
-
 const logo = document.querySelector('.logo');
 const rootHtml = document.documentElement;
+const distanceScrollTop = 100;
 
-window.addEventListener('resize', decidesLogoWidthOnScroll);
-window.addEventListener('load', decidesLogoWidthOnScroll);
-
-function decidesLogoWidthOnScroll() {
-  const isMatched = window.matchMedia(`(min-width: ${breakpoints.sm}px)`).matches;
-
-  if (isMatched) {
-    window.addEventListener('scroll', changeLogoWidth);
-  } else {
-    window.removeEventListener('scroll',changeLogoWidth)
-  }
-}
-
-function changeLogoWidth() {
+window.addEventListener('scroll', function() {
   const distance = rootHtml.scrollTop;
-
-  if (distance > 100  ) {
-    logo.classList.add('small')
+  if (distance > distanceScrollTop  ) {
+    logo.classList.add('small');
   } else {
     logo.classList.remove('small');
   }
-}
+});
 
-  const btn = document.getElementById('free');
-  const btnInner = btn.innerHTML;
+const btn = document.getElementById('free');
+const btnInner = btn.innerHTML;
+
+btn.addEventListener('click', async (e) => {
+  e.target.innerHTML = 'Cargando...';
+  const constr = customElements.get('pw-dialog');
   
-  btn.addEventListener('click', async (e) => {
-    e.target.innerHTML = 'Cargando...';
-    const constr = customElements.get('pw-dialog');
-    
-    if (constr === undefined) {
-      const { PwDialog } = await import('./PwDialog.js')
-      customElements.define(PwDialog.is, PwDialog);
-    }
+  if (constr === undefined) {
+    const { PwDialog } = await import('./PwDialog.js')
+    customElements.define(PwDialog.is, PwDialog);
+  }
 
-    btn.innerHTML = btnInner;
-  });  
+  btn.innerHTML = btnInner;
+});  
