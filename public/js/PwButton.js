f74db036c390be9372131a4d1edf45602e7da967
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2022-04-29 15:04:41 -0500

    refactor: indent shadow dom content for PwButton and delete unused js file

--- public/js/PwButton.js
+++ public/js/PwButton.js
@@ -8,59 +8,61 @@ class PwButton extends HTMLElement {
     this.attachShadow({ mode: 'open' });
     this.shadowRoot.innerHTML = `
       <style>
-      :host {
-        display: inline-block;
-        position: relative;
-        background-color: transparent;
-        font-size: 2rem;
-        padding: 0;
-        border: none;
-        cursor: pointer;
-        outline: none;
-        user-select: none;
-        -webkit-tap-highlight-color: transparent;
-      }
-      
-      :host(:hover) .front {
-        transform: translateY(-4px);
-      }
-      
-      :host(:active) .front {
-        transform: translateY(0px);
-      }
-      
-      .layer {
-        display: inline-block;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        position: absolute;
-        border-radius: 12px;
-      }
-      
-      .deep {
-        background: var(--pw-button-bg-deep, linear-gradient(43deg, #4158D0 0%, #C850C0 100%));
-        transform: translateY(2px);
-      }
-      
-      .shadow {
-        background: var(--pw-button-bg-deep, linear-gradient(43deg, #4158D0 0%, #C850C0 100%));
-        transform: translateY(4px);
-        filter: blur(4px);
-      }
-      
-      .front {
-        display: inline-block;
-        position: relative;
-        padding: 1rem;
-        transition: transform 0.1s;
-        background-color: white;
-        border-radius: 12px;
-        transform: translateY(-6px);
-        border: 1px solid rgba(0, 0, 0, 0.2);
-      }
+        :host {
+          display: inline-block;
+          position: relative;
+          background-color: transparent;
+          font-size: 2rem;
+          padding: 0;
+          border: none;
+          cursor: pointer;
+          outline: none;
+          user-select: none;
+          -webkit-tap-highlight-color: transparent;
+          color: black;
+        }
+        
+        :host(:hover) .front {
+          transform: translateY(-4px);
+        }
+        
+        :host(:active) .front {
+          transform: translateY(0px);
+        }
+        
+        .layer {
+          display: inline-block;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          position: absolute;
+          border-radius: 12px;
+        }
+        
+        .deep {
+          background: var(--pw-button-bg-deep, linear-gradient(43deg, #4158D0 0%, #C850C0 100%));
+          transform: translateY(2px);
+        }
+        
+        .shadow {
+          background: var(--pw-button-bg-deep, linear-gradient(43deg, #4158D0 0%, #C850C0 100%));
+          transform: translateY(4px);
+          filter: blur(4px);
+        }
+        
+        .front {
+          display: inline-block;
+          position: relative;
+          padding: 1rem;
+          transition: transform 0.1s;
+          background-color: white;
+          border-radius: 12px;
+          transform: translateY(-6px);
+          border: 1px solid rgba(0, 0, 0, 0.2);
+        }
       </style>
+
       <span class="layer shadow"></span>
       <span class="layer deep"></span>
       <span class="front">
