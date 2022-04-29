dffba28ad3276e97c7c7b58b2031936a1dd3892c
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2022-04-29 15:14:28 -0500

    refactor: change logo nimatio only for screens greater than 700px

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,47 @@
+const breakpoints = {
+  sm: 700
+}
 const logo = document.querySelector('.logo');
 const rootHtml = document.documentElement;
 const distanceScrollTop = 100;
 
-window.addEventListener('scroll', function() {
+window.addEventListener('resize', decidesLogoWidthOnScroll);
+window.addEventListener('load', decidesLogoWidthOnScroll);
+preparePwDialog();
+
+function decidesLogoWidthOnScroll() {
+  const isMatched = window.matchMedia(`(min-width: ${breakpoints.sm}px)`).matches;
+
+  if (isMatched) {
+    window.addEventListener('scroll', changeLogoWidth);
+  } else {
+    window.removeEventListener('scroll', changeLogoWidth);
+  }
+}
+
+function changeLogoWidth() {
   const distance = rootHtml.scrollTop;
+  
   if (distance > distanceScrollTop  ) {
     logo.classList.add('small');
   } else {
     logo.classList.remove('small');
   }
-});
+}
 
-const btn = document.getElementById('free');
-const btnInner = btn.innerHTML;
+function preparePwDialog() {
+  const btn = document.getElementById('free');
+  const btnInner = btn.innerHTML;
 
-btn.addEventListener('click', async (e) => {
-  e.target.innerHTML = 'Cargando...';
-  const constr = customElements.get('pw-dialog');
-  
-  if (constr === undefined) {
-    const { PwDialog } = await import('./PwDialog.js')
-    customElements.define(PwDialog.is, PwDialog);
-  }
+  btn.addEventListener('click', async (e) => {
+    e.target.innerHTML = 'Cargando...';
+    const constr = customElements.get('pw-dialog');
+    
+    if (constr === undefined) {
+      const { PwDialog } = await import('./PwDialog.js')
+      customElements.define(PwDialog.is, PwDialog);
+    }
 
-  btn.innerHTML = btnInner;
-});  
+    btn.innerHTML = btnInner;
+  }); 
+} 
