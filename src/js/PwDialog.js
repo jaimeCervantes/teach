cbc80bba83930b25cfafff1b2ab5fcfe945be7ca
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2022-05-13 07:25:26 -0500

    feat: syncronize open attr and prop, center pw-dialog

--- src/js/PwDialog.js
+++ src/js/PwDialog.js
@@ -3,18 +3,71 @@ export class PwDialog extends HTMLElement {
     return 'pw-dialog';
   }
 
+  static get observedAttributes() {
+    return ['open'];
+  }
+
+  get open() {
+    return this.getAttribute('open');
+  }
+
+  set open(value) {
+    if (Boolean(value)) {
+      this.setAttribute('open', '');
+    } else {
+      this.removeAttribute('open');
+    }
+  }
+
+  attributeChangedCallback(attrName, prevVal, newVal) {
+    const attrHandler = this[`on_${attrName}Changed`].bind(this);
+    if (typeof attrHandler === 'function') {
+      attrHandler(attrName, prevVal, newVal);
+    }
+  }
+
+  on_openChanged() {
+    const dialog = this.shadowRoot.querySelector('dialog');
+    dialog.toggleAttribute('open');
+  }
+
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
-    this.shadowRoot.innerHTML = `
-      <style>
-        :host {
-          display: block;
-        }
-      </style>
-      <dialog open>
-        <h1>Hola</h1>
-      </dialog>
-    `;
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+
+  connectedCallback() {
+    // Add event listeners
+  }
+
+  disconnectedCallback() {
+    // remove event listeners
   }
 }
+
+
+const template = document.createElement('template');
+template.innerHTML = `
+  <style>
+    * {
+      box-sizing: border-box;
+    }
+
+    :host {
+      display: block;
+      position: fixed;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+
+    dialog {
+      width: 100%;
+      position: relative
+    }
+  </style>
+  <dialog>
+    <h1>Hola</h1>
+  </dialog>
+  `;
