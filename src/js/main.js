02f15e4b9d18f672e2807743cf1aa49b8fe9effc
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2022-06-13 13:39:37 -0500

    refactor: use btn variable ti reference pw-button instance

--- src/js/main.js
+++ src/js/main.js
@@ -7,13 +7,14 @@ function preparePwDialog() {
   const btn = document.getElementById('free');
 
   btn.addEventListener('click', async (e) => {
-    e.target.isLoading = true;
+    btn.isLoading = true;
     const constr = customElements.get('pw-dialog');    
+    
     if (constr === undefined) {
       const { PwDialog } = await import('./PwDialog.js')
       customElements.define(PwDialog.is, PwDialog);
     }
 
-    e.target.isLoading = false;
+    btn.isLoading = false;
   }); 
 } 
